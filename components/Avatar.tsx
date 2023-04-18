22bc03bfc2bae2891bb5b4dae38044364277f7e4
Steven Loh <88935273+getloh@users.noreply.github.com>
2023-04-18 23:33:15 +0100

    transition effect on hover for avatar

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -31,8 +31,9 @@ export default function Avatar(props: any)
     }
 
     const avatarSideClass = classNames(
-        "border-2 border-slate-200 w-8",
-        hover ? "block" : "hidden"
+        "border-2 border-slate-200 transition-all bg-slate-600 overflow-hidden",
+        // hover ? "block" : "hidden",
+        hover ? "w-8": "w-0"
     )
 
     return (
@@ -41,7 +42,7 @@ export default function Avatar(props: any)
         onMouseLeave={hoverOut}
         >
             <div className={avatarSideClass}>
-
+                <p>TEST</p>
             </div>
             <div className="w-16 aspect-square border-2 border-red-900 p-1 cursor-pointer">
                 <div className="bg-lime-900 h-full w-full"
